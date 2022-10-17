aed6695d9d4afa020b3fc098c536ef82cdc11339
Anastasia Korobkina <a.korobkina.19@gmail.com>
2022-10-17 20:44:20 +0300

    Update model.py

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,10 +1,10 @@
-from transformers import AutoModelWithLMHead, AutoTokenizer, pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
  
 class TransformerTranslator(object):
     def __init__(self,  max_length=5000, model=None, tokenizer=None,):
         self.max_length = max_length
-        self.model = AutoModelWithLMHead.from_pretrained("Helsinki-NLP/opus-mt-en-et")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-et")
         self.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-et")
         self.pipe = pipeline("translation_en_to_et", model=self.model, tokenizer=self.tokenizer)
 
